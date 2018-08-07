9fd71f1330e95eb6c10036d7b746c9f40d68aaac
Haijun Wang <wanghj@woosiyuan.com>
2018-08-07 15:05:54 +0800

    update common_proc for pre-pre-process

--- common_proc.py
+++ common_proc.py
@@ -14,23 +14,34 @@ from projective_transform import affine_transform
 GAUSSIAN_SMOOTH_FILTER_SIZE = (3, 3)
 BINARY_THRESH = 160
 
-folder = "./entire_image/2"
+im_file = "./entire_image/3/222.jpg"
+save_image= "./entire_image/3_out/222.jpg"
+mask_file = ["./entire_image/3/222_11.jpg", "./entire_image/3/222_12.jpg", "./entire_image/3/222_13.jpg"]
 
-for im_name in os.listdir(folder):
-    im_file = os.path.join(folder, im_name)
-    im = cv2.imread(im_file)
 
-    gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
-    mean = np.mean(gray)
-    brightness = (1-mean/255) * 3
+print("reading {}".format(im_file))
+im = cv2.imread(im_file)
 
-    image = Image.fromarray(im)
-    # enhance brightness
-    image = ImageEnhance.Brightness(image).enhance(brightness)
+brightness = []
+for im_f in mask_file:
+    image = cv2.imread(im_f)
+    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    v = hsv[:,:,2]
+    mean = np.mean(v)
 
-    # save image
-    image_save = np.array(image)
-    cv2.imwrite(os.path.join(folder, im_name.split('.')[0]+"_out.jpg"), image_save)
+    brightness.append(mean)
+
+bright = np.mean(np.array(brightness))
+print("brightness: {}".format(bright))
+bright = (1-bright/255) * 3
+
+image = Image.fromarray(im)
+# enhance brightness
+image = ImageEnhance.Brightness(image).enhance(bright)
+
+# save image
+image_save = np.array(image)
+cv2.imwrite(save_image, image_save)
 
 
     # enhance contrast
@@ -41,7 +52,3 @@ for im_name in os.listdir(folder):
     # curve adjust
     # im = np.array(im)
     # im = curve_adjust(os.path.join(os.path.dirname(__file__), 'curve.acv'), im)
-
-    # to gray
-    # hsv = cv2.cvtColor(im, cv2.COLOR_BGR2HSV)
-    # hue, saturation, value = cv2.split(hsv)
